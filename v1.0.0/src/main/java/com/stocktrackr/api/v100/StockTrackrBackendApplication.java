c21a5afca05e65d8b3ef8f0094a184836f535ab4
akashChatterjee1999 <akashChatterjee1000@gmail.com>
2023-09-24 15:58:25 +0530

    Some commit to verify the commit sign

--- v1.0.0/src/main/java/com/stocktrackr/api/v100/StockTrackrBackendApplication.java
+++ v1.0.0/src/main/java/com/stocktrackr/api/v100/StockTrackrBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StockTrackrBackendApplication {
 
+	// the main method the application
 	public static void main(String[] args) {
 		SpringApplication.run(StockTrackrBackendApplication.class, args);
 	}
