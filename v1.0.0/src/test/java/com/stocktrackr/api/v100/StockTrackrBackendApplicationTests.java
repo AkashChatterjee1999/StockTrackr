a390deb50344bc072aea1b4fadcb836209b2a18d
akash.chatterjee <akash@vitto.money>
2023-09-24 15:54:46 +0530

    Initial commit

---
+++ v1.0.0/src/test/java/com/stocktrackr/api/v100/StockTrackrBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.stocktrackr.api.v100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
