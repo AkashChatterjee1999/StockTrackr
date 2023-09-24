6bcafefb9540f5d013050efcce9bdacf736fc396
akash.chatterjee <akash@vitto.money>
2023-09-24 15:54:46 +0530

    Initial commit

---
+++ v1.0.0/src/main/java/com/stocktrackr/api/v100/StockTrackrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stocktrackr.api.v100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrackrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackrBackendApplication.class, args);
+	}
+
+}
